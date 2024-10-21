41b5efec7ec7518c057e30fb8325cb2b9fbae025
Meridolian <meridolian@proton.me>
2024-10-21 22:33:59 +0200

    fix: isFullScreen

--- src/WindowInverter.cpp
+++ src/WindowInverter.cpp
@@ -190,7 +190,7 @@ bool WindowInverter::MatchesDeprecatedRule(PHLWINDOW window)
             }
 
             if (rule.bFullscreen != -1) {
-                if (window->isFullscreen() != rule.bFullscreen)
+                if (window->m_bIsFullscreen() != rule.bFullscreen)
                     continue;
             }
 
